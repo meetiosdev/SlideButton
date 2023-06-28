a3d996845c966d83b03b1a1051d7dc25b25d6da1
swarajsinghdev <swarajmeet.crebos@gmail.com>
2023-06-29 00:37:52 +0530

    next

--- slideButton/ContentView.swift
+++ slideButton/ContentView.swift
@@ -7,24 +7,26 @@ struct SlideToUnlockButton: View {
     var body: some View {
         HStack {
             ZStack(alignment: .leading) {
-                if xOffset >= 200 {
-                    Text("Unlocked")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                        .transition(.opacity)
-                } else {
-                    Text("Slide to Unlock")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding()
-                        .transition(.opacity)
-                }
-                
                 GeometryReader { geometry in
+                    if xOffset >= 200 {
+                        Text("Unlocked")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .transition(.opacity)
+                            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                    } else {
+                        Text("Slide to Unlock")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .transition(.opacity)
+                            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                    }
+                    
                     Circle()
                         .fill(Color.green)
-                        .frame(width: 60, height: 60)
+                        .frame(width: 50, height: 50)
                         .position(x: min(max(xOffset + 30, 30), geometry.size.width - 30), y: geometry.size.height / 2)
                         .gesture(DragGesture()
                                     .onChanged { value in
@@ -49,6 +51,7 @@ struct SlideToUnlockButton: View {
         .background(Color.black)
         .cornerRadius(30)
         .frame(height: 60)
+        .padding(5)
         .animation(.spring())
         .padding()
     }
