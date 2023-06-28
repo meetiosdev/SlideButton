ed5f03050e68cd21d40e7e34d9792eb9c68cd73f
swarajsinghdev <swarajmeet.crebos@gmail.com>
2023-06-28 23:00:40 +0530

    Initial Commit

---
+++ slideButton/slideButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  slideButtonApp.swift
+//  slideButton
+//
+//  Created by Swarajmeet Singh on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct slideButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
