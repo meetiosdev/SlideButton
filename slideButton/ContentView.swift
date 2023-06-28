d45d3599212c0c96e3d7821d2a71c334a2d2d04b
swarajsinghdev <swarajmeet.crebos@gmail.com>
2023-06-29 00:20:39 +0530

    first commit

--- slideButton/ContentView.swift
+++ slideButton/ContentView.swift
@@ -1,21 +1,59 @@
-//
-//  ContentView.swift
-//  slideButton
-//
-//  Created by Swarajmeet Singh on 28/06/23.
-//
-
 import SwiftUI
+
+struct SlideToUnlockButton: View {
+    @State private var xOffset: CGFloat = 0
+    @State private var unlocked = false
+    
+    var body: some View {
+        HStack {
+            ZStack(alignment: .leading) {
+                if xOffset >= 200 {
+                    Text("Unlocked")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .transition(.opacity)
+                } else {
+                    Text("Slide to Unlock")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .transition(.opacity)
+                }
+                
+                Circle()
+                    .fill(Color.green)
+                    .frame(width: 60, height: 60)
+                    .offset(x: xOffset)
+                    .gesture(DragGesture()
+                                .onChanged { value in
+                                    let newOffset = xOffset + value.translation.width
+                                    xOffset = max(0, min(newOffset, 200))
+                                }
+                                .onEnded { value in
+                                    if xOffset > 200 {
+                                        unlocked = true
+                                    } else {
+                                        xOffset = 0
+                                        unlocked = false
+                                    }
+                                }
+                    )
+            }
+            
+            Spacer()
+        }
+        .background(Color.black)
+        .cornerRadius(10)
+        .frame(height: 80)
+        .animation(.spring())
+        .padding()
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        SlideToUnlockButton()
     }
 }
 
